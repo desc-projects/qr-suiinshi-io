b09e02dfdc0e3dc3bc86d5c09f834e37ae08ff40
ba-mirza <almas1610@gmail.com>
2025-02-22 01:08:46 +0500

    [fix]: adaptability for different size devices

--- src/App.tsx
+++ src/App.tsx
@@ -85,23 +85,26 @@ function App() {
             <i className="flex items-center fi fi-br-search scale-200"></i>
           </Button>
           <Popover className="
-          rounded-2xl h-[104px] w-[408px]
+          flex items-center
+          rounded-2xl h-[104px] w-[90vw]
+          max-w-[390px]
           bg-white drop-shadow-[0_-6px_18.8px_rgba(0,0,0,0.2)]
-          overflow-x-scroll p-1" backdrop="blur" placement="top" offset={30}>
+          overflow-x-scroll p-1" backdrop="blur" placement="top" offset={25}>
             <PopoverTrigger>
               <Button>
                 <i className="flex items-center fi fi-br-menu-burger scale-200"></i>
               </Button>
             </PopoverTrigger>
-            <PopoverContent>
+            <PopoverContent className="p-0">
               <div className="flex items-center justify-between w-full">
               {
                 Array.from({length: 10}).map((_, idx) => (
-                  <div key={idx}
-                  className="m-5 p-1">
-                    <i className="flex items-center fi fi-rs-holding-hand-dinner scale-300"></i>
-                    Pizza
-                  </div>
+                  <Button
+                  key={idx}
+                  disableRipple
+                  className="flex m-5 p-1">
+                  Pizza
+                  </Button>
                 ))
               }
               </div>
