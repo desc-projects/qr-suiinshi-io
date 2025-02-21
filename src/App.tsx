3fff54e3ac52dc12554c9e3497a5fb60e55db888
ba-mirza <almas1610@gmail.com>
2025-02-21 14:55:49 +0500

    [deps]: added heroui&framer-motion, tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
+import {Button, Popover, PopoverContent, PopoverTrigger} from "@heroui/react";
 
 function App() {
 
   return (
     <>
-      <h1>SUUINSHI MENU APP</h1>
+      <h1 className='underline'>SUUINSHI MENU APP</h1>
+      <Popover placement="right">
+        <PopoverTrigger>
+          <Button className='underline'>Open Popover</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="px-1 py-2">
+            <div className="text-small font-bold">Popover Content</div>
+            <div className="text-tiny">This is the popover content</div>
+          </div>
+        </PopoverContent>
+      </Popover>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import {HeroUIProvider} from "@heroui/react";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HeroUIProvider>
+      <App />
+    </HeroUIProvider>
   </StrictMode>,
 )
