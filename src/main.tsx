21e88d3eff100f2cf985501cbbc06f2dddc57aed
ba-mirza <almas1610@gmail.com>
2025-02-23 16:52:31 +0500

    [deps]: added react-router for routing between pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-import {HeroUIProvider} from "@heroui/react";
+import { HeroUIProvider } from "@heroui/react";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HeroUIProvider>
-        <App />
-    </HeroUIProvider>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <HeroUIProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </HeroUIProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
